1760ac87a724e9313a7bb44176e5ec4d6a60d396
moon <d.moon23k@gmail.com>
2024-04-19 22:23:44 +0900

    Initialized

--- setup.py
+++ setup.py
@@ -1,73 +1,53 @@
-import os, json, nltk
-from datasets import load_dataset
-from transformers import BertTokenizerFast
+import os, json, zipfile, unicodedata
+from tqdm import tqdm
 
 
 
-#Select and Tokenize Data
-def process_data(orig_data, tokenizer):
+def extract_zip_file_names():
+    trg_dirs = []
+    fnames = []
 
-    processed = []
-    cnt, volumn = 0, 34000
-    min_len, max_len = 1000, 3000
+    #get dir info and save it to lst
+    for root, dirs, files in os.walk('data'):
+        norm_root = unicodedata.normalize('NFC', root)
+        if '라벨링' in norm_root:
+            trg_dirs.append(norm_root)
 
-    
-    for elem in orig_data:
-        src, trg = elem['article'].lower(), elem['highlights'].lower()
-
-        #Filter too Short or too Long Context
-        if not (min_len < len(src) < max_len):
-            continue
-
-        src = nltk.tokenize.sent_tokenize(src)
-        src = tokenizer(src).input_ids
-        
-        temp_ids, temp_segs = [], []
-        for idx, ids in enumerate(src):
-            _len = len(ids)
-            
-            #Add ids
-            temp_ids.extend(ids)
-            
-            #Add segs
-            if not idx % 2:
-                temp_segs.extend([0 for _ in range(_len)])
-            else:
-                temp_segs.extend([1 for _ in range(_len)])
-
-        processed.append({"input_ids": temp_ids,
-                          "token_type_ids": temp_segs,
-                          "labels": tokenizer(trg).input_ids})
-        
-        cnt += 1
-        if cnt == volumn:
-            break
-
-    return processed
+    for dir in trg_dirs:
+        for f in os.listdir(dir):
+            if 'en' in f:
+                fnames.append('/'.join([dir, f]))
+    return fnames
 
 
 
-def save_data(data_obj):
-    #split data into train/valid/test sets
-    train, valid, test = data_obj[:-4000], data_obj[-4000:-1000], data_obj[-1000:]
-    data_dict = {k:v for k, v in zip(['train', 'valid', 'test'], [train, valid, test])}
+def concat_data(zip_file_names):
+    concatenated = []
 
-    for key, val in data_dict.items():
-        with open(f'data/{key}.json', 'w') as f:
-            json.dump(val, f)                    
+    for zip_file in tqdm(zip_file_names):
+        with zipfile.ZipFile(zip_file, 'r') as data_dir:
+            for data_file in data_dir.infolist():
+                with data_dir.open(data_file) as file:
+                    data_str = file.read().decode('utf-8')
+                    data_dict = json.loads(data_str)
+
+                    lang_pair = f"{data_dict['sourceLanguage']}{data_dict['targetLanguage']}"
+                    x = data_dict['sourceText'] if lang_pair == 'enko' else data_dict['targetText']
+                    y = data_dict['targetText'] if lang_pair == 'enko' else data_dict['sourceText']
+
+                    sample = {
+                        'domain': data_dict['domain'],
+                        'style': data_dict['style'],
+                        'x': x,
+                        'y': y
+                    }
+
+                    concatenated.append(sample)
+
+    return concatenated
 
 
 
 def main():
-    orig = load_dataset('cnn_dailymail', '3.0.0', split='train')
-    tokenizer = BertTokenizerFast.from_pretrained('prajjwal1/bert-small')
-    tokenizer.model_max_length = 1024
-
-    processed = process_data(orig, tokenizer)    
-    save_data(processed)
-
-
-
-if __name__ == '__main__':
-    nltk.download('punkt')    
-    main()
+    zip_file_names = extract_zip_file_names()
+    concatenated = concat_data(zip_file_names)    
